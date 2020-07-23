c9cc9d5bbf44aec25b071818e9b45b6d2275d024
james <sj2@penrose.us>
2020-07-23 14:18:50 -0700

    Moved some images. Formatted robot page.

--- src/members/profiles.js
+++ src/members/profiles.js
@@ -10,7 +10,7 @@ var people = [{
         "Favorite food": "Korean BBQ"
         },
     bio:"James has done FLL for two years on team 32221 Thunder Chickens as a programmer and builder, the team went to LA regionals twice and won a programming award and numerous design awards. He is excited to now start FTC. He enjoys doing kickboxing and Brazilian Ju-Jitsu. He is learning to be a Programmer and Robot designer.",
-    imageUrl:"../images/profileJames.png"
+    imageUrl:"images/profileJames.png"
 },{
 
 
@@ -23,7 +23,7 @@ var people = [{
         "Favorite food": "Soy yogurt",
         },
     bio:"Adrian is a part of Trinity Pacific Christian School and this is his first year in FTC. Last year he was a part of FLL with a team called, “Bricking the Limits” who made it to regionals and now he will be a part of FTC for the next four years. Adrian likes to play video games and plays Piano. He is excited to be a part of Brain Stormz and wants to learn about technology, problem solving, programing, and teamwork.",
-    imageUrl:"../images/profileAdrian.png"
+    imageUrl:"images/profileAdrian.png"
 },{
 
 
@@ -34,7 +34,7 @@ var people = [{
         "Favorite Food": "σαγανάκι",
         },
     bio: "A life-long hardware &amp; software engineering enthusiast with a passion for teaching, Stu has worked as a product &amp; software engineering leader in a variety of large-scale internet companies over the past 20 years.&nbsp; When he's not coding or hanging out with Brain Stormz, you'll find him flying, sailing, or hanging out with his wife and 4 kids.<br>" + 'Learn more about Stu at&nbsp;<a href="http://stu.penrose.us" target="_blank" rel="noopener">stu.penrose.us</a>',
-    imageUrl:"../images/profileStu.jpg"
+    imageUrl:"images/profileStu.jpg"
 },{
 
 
@@ -45,7 +45,7 @@ var people = [{
         "Age": "25"
     },
     bio:"Garret is a mechanical engineer, and mentors us in mechanical design, fabrication, and CAD.",
-    imageUrl:"../images/profileGarrett.jpg"
+    imageUrl:"images/profileGarrett.jpg"
 },{
 
 
@@ -58,7 +58,7 @@ var people = [{
         "Favorite food": "Soy yogurt"
     },
     bio:"Jacob is a Nerd/Geek (he hasn’t figured out which one yet) who has been interested in technology his entire life.&nbsp;This is his 3rd year on a FIRST team. He has his own computer repair business, Jacob’s PC Tune-Up.&nbsp; When not working on robots or robot code, Jacob enjoys playing/contributing to/telling people about SuperTuxKart way too much. He is a senior homeschooler.",
-    imageUrl:"../images/profileJacob.jpg"
+    imageUrl:"images/profileJacob.jpg"
 },{
 
 
@@ -71,7 +71,7 @@ var people = [{
         "Favorite food": "Skyline Chili"
     },
     bio: "Destin attends Trinity Pacific Christian School. He became interested in FIRST when he started a new FLL team, Bricking the Limits 45558, and won 2nd Robot Design at Q.T. and 1st Core Values at Regionals. He is excited to be part of an FTC team and learn. He loves technology and wants to be an engineer, he also plays soccer for a local community league.",
-    imageUrl: "../images/profileDestin.png"
+    imageUrl: "images/profileDestin.png"
 },{
 
 
@@ -82,7 +82,7 @@ var people = [{
         "Favorite food": "Street Tacos"
     },
     bio: "Glenn has coached FIRST teams since 2019. He has an Electrical Engineering degree from the University of Cincinnati. Glenn is passionate about technology, loves to build and teach. He is married with 3 children. ",
-    imageUrl: "../images/profileGlenn.jpg"
+    imageUrl: "images/profileGlenn.jpg"
 },{
 
 
@@ -94,7 +94,7 @@ var people = [{
         "Favorite food": "Fondue"
     },
     bio: "Jeff has been coaching in FIRST for five years, and this is his third year coaching an FTC team. Jeff has been the owner of a growing automated shipping business for the past 11 years. He graduated college with a bachelor’s degree in architecture from Cal Poly. Jeff is a proud husband and father of three kids at 18, 16, and 12 years old. He has a strong background in sales marketing, fundraising, and business planning. Jeff is excited to be coaching Brain Stormz and looks forward to their future success.",
-    imageUrl: "../images/profileJeff.png"
+    imageUrl: "images/profileJeff.png"
 },{
 
 
@@ -106,7 +106,7 @@ var people = [{
         "Favorite food": "Asian Confusion"
     },
     bio: "Sung has coached FIRST teams since FTC Block Party. Sung has been a software/systems engineer for over 20 years in various industries. He has a Masters degree from USC in Electrical Engineering. He is a loving husband and father of 2 children. Sung has a strong background in Software, Systems Design and Electrical Engineering.",
-    imageUrl: "../images/profileSung.jpg"
+    imageUrl: "images/profileSung.jpg"
 },{
 
 
@@ -119,7 +119,7 @@ var people = [{
         "Favorite food": "Korean BBQ"
     },
     bio: "Stephanie has found a strong passion in being a part of FIRST. She started participating in FIRST by joining an FLL team. From this she moved onto FTC. Stephanie has found an interest in the field of STEM and plans to purse a career in it. Stephanie has experience in programming and mechanical. This is her 4th year in FTC. She is currently a senior at Granada Hills Charter High School.",
-    imageUrl: "../images/profileStephanie.jpg"
+    imageUrl: "images/profileStephanie.jpg"
 },{
 
 
@@ -131,7 +131,7 @@ var people = [{
         "Favorite food": "Fish and Chips"
     },
     bio: "Daniel Peace is 19 years old and has always been interested in robotics and machines. This is his fourth year in First Tech Challenge. He was one of Brain Stormz’s founding members, and is now a mentor. When not helping with robots he enjoys playing basketball and volleyball, and he also plays piano.",
-    imageUrl: "../images/profileDaniel.jpg"
+    imageUrl: "images/profileDaniel.jpg"
 },{
 
 
@@ -144,7 +144,7 @@ var people = [{
         "Favorite food": "Burgers"
     },
     bio: "Noah has been involved in FIRST programs since 2013. This is Noah’s fourth year on a FTC team, having completed two years prior of FLL with The B.E.S.T Team and being a founding member of the FTC team Brain Stormz. Noah is part of Trinity Pacific Christian School, which is a private school in Thousand Oaks, California. He is heavily involved in basketball at his school having played since he was ten. In his free time he likes to play his collection of vintage video games including a Sega Genesis and a Nintendo Entertainment System. Noah has liked Engineering since he was little having been a fan of Lego/Mindstorms since he was 7.",
-    imageUrl: "../images/profileNoah.png"
+    imageUrl: "images/profileNoah.png"
 },{
 
 
@@ -157,7 +157,7 @@ var people = [{
         "Favorite food": "Shin Ramen"
     },
     bio: "Nathan Choi is a 16 year nerd who goes to Trinity Pacific School and is in his 6th year of FIRST competitions and 5th year of FTC. He was a member of FTC Trinity Crown Bots when he was a 6th grader. After that, he joined the FLL team, The B.E.S.T. Team. Nathan enjoys reading, playing with Legos, and playing video games. He is excited for Rover Ruckus, and is ready for a great season.",
-    imageUrl: "../images/profileNathan.jpg"
+    imageUrl: "images/profileNathan.jpg"
 },{
 
 
@@ -170,7 +170,7 @@ var people = [{
         "Favorite food": "Pizza"
     },
     bio: "Ian is a person who loves to build and do things with his hands. This is his 1st year on a FIRST team. He likes to work on and fix up go-karts. He is a 9th grade homeschooler.",
-    imageUrl: "../images/profileIan.jpg"
+    imageUrl: "images/profileIan.jpg"
 },{
 
 
@@ -183,7 +183,7 @@ var people = [{
         "Favorite food": "Sushi"
     },
     bio: "Olivia has been involved in FIRST since 2017, and this is her second year on an FTC team. She has loved building since she was little and plans on pursuing a career in engineering. In her free time, Olivia enjoys reading and playing tennis. She is a senior at West Ranch High School.",
-    imageUrl: "../images/profileOlivia.jpg"
+    imageUrl: "images/profileOlivia.jpg"
 },{
 
 
@@ -196,7 +196,7 @@ var people = [{
         "Favorite food": "Enchiladas"
     },
     bio: "David is a senior at Trinity Pacific High School. He has been interested in FIRST ever since 8th grade, when he was a part of a FLL team with some other members of Brain Stormz. He is aspiring to become a District Attorney when he grows up, and perhaps pursue a career in politics from there. Also, he loves to debate and to participate in Mock Trial.",
-    imageUrl: "../images/profileDavid.jpg"
+    imageUrl: "images/profileDavid.jpg"
 },{
 
 
@@ -209,7 +209,7 @@ var people = [{
         "Favorite food": "Burritos"
     },
     bio: "Joshua is a freshman who attends Trinity Pacific Christian School. He became interested in FIRST when he attended a match as a guest with the 2015-2016 Brain Stormz team. He is currently on the Trinity Pacific basketball and volleyball team.",
-    imageUrl: "../images/profileJoshua.jpg"
+    imageUrl: "images/profileJoshua.jpg"
 },{
 
 
@@ -235,7 +235,7 @@ var people = [{
         "Favorite food": "Apple dumplings!"
     },
     bio: "Kimberly Sharp truly enjoys being on team Brain Stormz. She has been a part of FLL team B.E.S.T. team for two years before this and really loved it. However, in her 10th grade year she feared she had too large of a school load to take on the extra challenge of FTC, deciding not to be on the team. Last year, she came back, not because of a lighter school load, but because she missed it so much she is willing to risk overload in order to be on such a great team as Brain Stormz. She knows from experience that it is well worth it.",
-    imageUrl: "../images/profileKimberly.jpg"
+    imageUrl: "images/profileKimberly.jpg"
 },{
 
 
@@ -248,7 +248,7 @@ var people = [{
         "Favorite food": "Burritos"
     },
     bio: "Skyler Atchison is a 14-year-old homeschooled roboticist. He has been with the team for three years and is excited to be doing FTC. His other interests include mime, reading, writing, video games, board games, and video production. He prides himself on being able to pronounce Pneumonoultramicroscopicsilicovolcanoconiosis... and even if he isn’t pronouncing it right, nobody can prove him wrong.",
-    imageUrl: "../images/profileSkyler.png"
+    imageUrl: "images/profileSkyler.png"
 }]
 
 
