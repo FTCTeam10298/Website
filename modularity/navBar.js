d88f1391b6d3ead2e6de570646cb15e54ad87d12
james <sj2@penrose.us>
2020-07-30 22:08:34 -0700

    Finished smallnav.

--- modularity/navBar.js
+++ modularity/navBar.js
@@ -1,42 +1,28 @@
-    let tog = "flex"
-    let menu = document.getElementById('menu');
-    let bar = document.getElementById("bar");
-
-    function pullDom() {
-        menu = document.getElementById('menu');
-        bar = document.getElementById("bar");
-        // let bar = document.querySelector('my-element');
-    }
-
-    while (menu == null || bar == null) {
-        pullDom // try again in 300 milliseconds
-    }
-
-
-    console.log(bar)
-
-    bar.addEventListener("click", showMenu);
-
+    let tog = "none";
     function showMenu() {
-        console.log("click")
-        switch (tog) {
-            case("block"): tog = "none"; break;
 
+        let menu = document.getElementById('menu');
+
+        console.log("click");
+
+        switch (tog) {
             default: tog = "block"; break;
+
+            case("block"): tog = "none"; break;
         }
 
         menu.style.display = tog;
-
     };
 
-    // window.addEventListener("resize", function() {
-    //     console.log("change")
 
-    //     if (window.innerWidth >= 901) {
-    //         menu.style.display = "flex";
-    //         tog = "none";
-    //     } else {
-    //         menu.style.display = tog;
-    //     }
+    window.addEventListener("resize", function() {
+        console.log("change")
 
-    // });
+        if (window.innerWidth >= 901) {
+            menu.style.display = "flex";
+            tog = "none";
+        } else {
+            menu.style.display = tog;
+        }
+
+    });
